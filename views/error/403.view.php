1f1ef7439f475f5c76f32bd072d6808900268770
t0nn1x <antoonhrobust@gmail.com>
2024-02-13 18:20:43 +0100

    Refactored routing structure

---
+++ views/error/403.view.php
@@ -0,0 +1,7 @@
+<?php loadPartial('head') ?>
+
+<?php loadPartial('navbar') ?>
+
+<?php loadPartial('403') ?>
+
+<?php loadPartial('footer') ?>
---
+++ views/partials/403.php
@@ -0,0 +1,15 @@
+<!-- 404 Start -->
+<div class="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
+  <div class="container text-center">
+    <div class="row justify-content-center">
+      <div class="col-lg-6">
+        <i class="bi bi-exclamation-triangle display-1 text-primary"></i>
+        <h1 class="display-1">403</h1>
+        <h1 class="mb-4">You are not authorized to view this page</h1>
+        <p class="mb-4">We're sorry, but you are not authorized to view this page. Please go back to our home page or try using the search function.</p>
+        <a class="btn btn-primary py-3 px-5" href="/">Go Back To Home</a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- 404 End -->
