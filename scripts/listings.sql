97bbf087695f3b6fdd7896926ef51ca34e77956b
t0nn1x <antoonhrobust@gmail.com>
2024-02-14 20:14:27 +0100

    Added employment_type column for listings

--- scripts/listings.sql
+++ scripts/listings.sql
@@ -1,4 +1,3 @@
--- Active: 1707847341122@@127.0.0.1@3306@job_portal
 CREATE TABLE `listings` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
@@ -16,9 +15,10 @@ CREATE TABLE `listings` (
   `benefits` longtext,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `category_id` int DEFAULT NULL,
+  `employment_type` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_listings_users` (`user_id`),
   KEY `fk_listings_categories` (`category_id`),
   CONSTRAINT `fk_listings_categories` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
   CONSTRAINT `fk_listings_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
+) ENGINE = InnoDB AUTO_INCREMENT = 28 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
