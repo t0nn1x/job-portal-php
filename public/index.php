9a53b9deda0cae85e5a777bcf4d44c60a16285d4
t0nn1x <antoonhrobust@gmail.com>
2024-02-16 22:38:14 +0100

    Created custom autoloader

--- public/index.php
+++ public/index.php
@@ -1,7 +1,12 @@
 <?php
 require '../helpers.php';
-require basePath('Framework/Database.php');
-require basePath('Framework/Router.php');
+
+spl_autoload_register(function ($class) {
+  $path = basePath('Framework/' . $class . '.php');
+  if(file_exists($path)) {
+    require $path;
+  }
+});
 
 // Instantiating the router
 $router = new Router();
