4b578122956b88a8093336578c315fe459734dd3
t0nn1x <antoonhrobust@gmail.com>
2024-02-20 20:19:31 +0100

    Changed route name to comply with the rest api naming convention

--- routes.php
+++ routes.php
@@ -3,4 +3,4 @@ $router->get('/', 'HomeController@index');
 $router->get('/listings', 'ListingController@index');
 $router->post('/listings', 'ListingController@store');
 $router->get('/listings/create', 'ListingController@create');
-$router->get('/listing/{id}', 'ListingController@show');
+$router->get('/listings/{id}', 'ListingController@show');
