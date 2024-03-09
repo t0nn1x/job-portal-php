71a7bafbdbf4e1962e1288e56f60be228af6a1b1
t0nn1x <antoonhrobust@gmail.com>
2024-03-09 20:56:07 +0100

    Changed title

--- App/views/partials/head.php
+++ App/views/partials/head.php
@@ -4,7 +4,7 @@
 <head>
     <base href="http://localhost:8000/">
     <meta charset="utf-8">
-    <title>JobEntry - Job Portal Website Template</title>
+    <title>JobJunction</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
